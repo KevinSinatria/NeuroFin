99b3307c060609027141b8f21ab8ba45afab10dc
Kevin Sinatria <kevinsinatria8@gmail.com>
2025-05-11 17:52:06 +0700

    Finalizing RESTful API

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://neurofin-be.vercel.app",
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
